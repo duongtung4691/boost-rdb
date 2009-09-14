dbff094a74307d29be3735dc35f97094546b6f1d
jl <jl@yorel.be>
2009-09-14 23:06:54 +0200

    begin odbc Ignore-this: 9a417e49601f7f0c215f02943517e8af

---
+++ boost/rdb/odbc.hpp
@@ -0,0 +1,8 @@
+#ifndef BOOST_ODBC_HPP
+#define BOOST_ODBC_HPP
+
+namespace boost { namespace rdb { namespace odbc {
+
+} } }
+
+#endif // BOOST_ODBC_HPP
---
+++ libs/rdb/test/test_odbc.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <sstream>
+#include <boost/test/minimal.hpp>
+#include <boost/rdb/rdb.hpp>
+#include <boost/rdb/odbc.hpp>
+
+using namespace std;
+using namespace boost::rdb;
+
+#define scope
+
+BOOST_RDB_BEGIN_TABLE(person) 
+  BOOST_RDB_COLUMN(id, integer)
+  BOOST_RDB_COLUMN(name, varchar<20>)
+BOOST_RDB_END_TABLE(person) 
+
+int test_main( int, char *[] )
+{
+  using namespace boost::rdb;
+  using namespace boost::rdb::odbc;
+      
+  return 0;
+}
