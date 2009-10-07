50b95268c56e20b5a2be9336b6c7f427b31d580f
jl <jl@yorel.be>
2009-10-07 18:06:27 +0200

    select_statement operator () also uses transition

--- boost/rdb/sql/detail/select_begin_call.hpp
+++ boost/rdb/sql/detail/select_begin_call.hpp
@@ -4,31 +4,19 @@
 #define n BOOST_PP_ITERATION()
 
     template<BOOST_PP_ENUM_PARAMS(n, class Expr)>
-    select_statement<
-      Subdialect,
+    typename transition<
       typename Subdialect::select::exprs,
-      typename result_of::add_key<
-        Data,
+      fusion::vector<
+        BOOST_PP_REPEAT(n, BOOST_RDB_PP_RESULT_OF_AS_EXPRESSION, Expr)
+      >
+    >::type
+    operator ()(BOOST_PP_ENUM_BINARY_PARAMS(n, const Expr, &expr)) {
+      return typename transition<
         typename Subdialect::select::exprs,
         fusion::vector<
           BOOST_PP_REPEAT(n, BOOST_RDB_PP_RESULT_OF_AS_EXPRESSION, Expr)
         >
-      >::type,
-      Subdialect
-    >
-    operator ()(BOOST_PP_ENUM_BINARY_PARAMS(n, const Expr, &expr)) {
-      return select_statement<
-        Subdialect,
-        typename Subdialect::select::exprs,
-        typename result_of::add_key<
-          Data,
-          typename Subdialect::select::exprs,
-          fusion::vector<
-            BOOST_PP_REPEAT(n, BOOST_RDB_PP_RESULT_OF_AS_EXPRESSION, Expr)
-          >
-        >::type,
-        Subdialect
-      >(add_key<typename Subdialect::select::exprs>(data_,
+      >::type(add_key<typename Subdialect::select::exprs>(data_,
         fusion::vector<
           BOOST_PP_REPEAT(n, BOOST_RDB_PP_RESULT_OF_AS_EXPRESSION, Expr)
         >(
