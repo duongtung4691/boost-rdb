768e1c6a2878c9779f96de8848fb1b93cb3170e1
jl <jl@yorel.be>
2009-10-12 22:59:32 +0200

    check that expression passed to `where` is a boolean

--- boost/rdb/sql/detail/select_where.hpp
+++ boost/rdb/sql/detail/select_where.hpp
@@ -3,6 +3,7 @@
     typename transition<typename Subdialect::where, Predicate>::type
     where(const Predicate& predicate) const {
       BOOST_MPL_ASSERT((allow<Subdialect, State, typename Subdialect::where>));
+      BOOST_CONCEPT_ASSERT((BooleanExpression<Predicate>));
       return typename transition<typename Subdialect::where, Predicate>::type(
         add_key<typename Subdialect::where>(this->data_, predicate));
     }
