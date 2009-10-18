62ca0ddf82358d31078d3c25283956603a0f8196
jl <jl@yorel.be>
2009-10-18 22:10:53 +0200

    dynamic placeholders from wrapper

--- boost/rdb/sql/dynamic_expression.hpp
+++ boost/rdb/sql/dynamic_expression.hpp
@@ -9,6 +9,9 @@
 namespace boost { namespace rdb { namespace sql {
 
   struct dynamic_placeholder { // make it a specialization of placeholder<> ? but what for ?
+    dynamic_placeholder(int type, int length) : type_(type), length_(length) { }
+    int type() const { return type_; }
+    int length() const { return length_; }
     int type_;
     int length_;
   };
@@ -17,13 +20,21 @@ namespace boost { namespace rdb { namespace sql {
   struct dynamic_expression_wrapper {
     typedef SqlType sql_type;
     
-    typedef fusion::vector< std::vector<dynamic_placeholder> > placeholder_vector;
-    placeholder_vector placeholders() const { return fusion::make_vector(std::vector<dynamic_placeholder>()); }
+    typedef fusion::vector< const std::vector<dynamic_placeholder> > placeholder_vector;
+    
+    placeholder_vector placeholders() const {
+      std::vector<dynamic_placeholder> t1 = impl_->placeholders_;
+      fusion::vector< const std::vector<dynamic_placeholder> > t2 = fusion::make_vector(t1);
+      return fusion::make_vector(impl_->placeholders_);
+    }
     
     enum { precedence = precedence_level::lowest };
+
     struct root {
       virtual void str(std::ostream& os) const = 0;
+      std::vector<dynamic_placeholder> placeholders_;
     };
+
     shared_ptr<root> impl_;
 
     dynamic_expression_wrapper(root* p) : impl_(p) { }
@@ -35,10 +46,27 @@ namespace boost { namespace rdb { namespace sql {
 
   template<class Expr>
   struct dynamic_expression_impl : dynamic_expression_wrapper<typename Expr::sql_type>::root {
-    dynamic_expression_impl(const Expr& expr) : expr_(expr) { }
+    typedef typename dynamic_expression_wrapper<typename Expr::sql_type>::root root;
+
+    dynamic_expression_impl(const Expr& expr) : expr_(expr) {
+      fusion::for_each(expr.placeholders(), make_placeholder(this->placeholders_));
+    }
+
+    struct make_placeholder {
+      make_placeholder(std::vector<dynamic_placeholder>& placeholders) : placeholders_(placeholders) { }
+      
+      mutable std::vector<dynamic_placeholder>& placeholders_;
+      
+      template<class Placeholder>
+      void operator ()(const Placeholder& p) const {
+        placeholders_.push_back(dynamic_placeholder(Placeholder::rdb_type::id, 1));
+      }
+    };
+
     virtual void str(std::ostream& os) const {
       expr_.str(os);
     }
+
     Expr expr_;
   };
 
