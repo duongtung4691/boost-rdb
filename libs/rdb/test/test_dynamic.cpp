a13d7c32b3a6d5736f7cc69c58558d72681a640c
jl <jl@yorel.be>
2009-10-22 18:03:25 +0200

    test for dynamic insert

--- libs/rdb/test/test_dynamic.cpp
+++ libs/rdb/test/test_dynamic.cpp
@@ -11,13 +11,19 @@ using namespace boost::rdb::sql::test::springfield;
 struct empty { };
 struct ptr { void* p; };
 
-BOOST_AUTO_TEST_CASE(test_dynamic_expression) {
+BOOST_AUTO_TEST_CASE(test_dynamic_expression_in_predicate) {
   person p("p");
 
   dynamic_boolean predicate = make_dynamic(p.age > 18);
   BOOST_RDB_CHECK_SQL(predicate, "p.age > 18");
 
   BOOST_RDB_CHECK_SQL(select(p.id).from(p).where(predicate), "select p.id from person as p where p.age > 18");
+}
+
+BOOST_AUTO_TEST_CASE(test_dynamic_expression_in_insert_columns) {
+  person p;
+  dynamic_integer col = make_dynamic(p.id);
+  BOOST_RDB_CHECK_SQL(insert_into(p)(col).values(1), "insert into person (id) values (1)");
 }
 
 BOOST_AUTO_TEST_CASE(test_dynamic_integer_placeholder) {
@@ -35,3 +41,12 @@ BOOST_AUTO_TEST_CASE(test_dynamic_varchar_placeholder) {
   BOOST_CHECK(fusion::at_c<0>(placeholders)[0].type() == rdb::type::varchar<20>::id);
   BOOST_CHECK(fusion::at_c<0>(placeholders)[0].length() == 20);
 }
+
+//BOOST_AUTO_TEST_CASE(test_dynamic_integer_placeholder_insert) {
+//  person p("p");
+//  dynamic_placeholder param = make_dynamic(_);
+//  fusion::vector< std::vector<dynamic_placeholder> > placeholders =
+//    (insert_into(p)(p.id).values(param)).placeholders();
+//  BOOST_CHECK(fusion::at_c<0>(placeholders)[0].type() == rdb::type::integer::id);
+//  BOOST_CHECK(fusion::at_c<0>(placeholders)[0].length() == 1);
+//}
