08fb0fb6a113dc9e019e8ab48bca8d9df795e65f
jl <jl@yorel.be>
2009-11-19 17:59:00 +0100

    remove constness of parameters

--- boost/rdb/odbc/detail/bind_parameters.hpp
+++ boost/rdb/odbc/detail/bind_parameters.hpp
@@ -4,7 +4,7 @@
 #define n BOOST_PP_ITERATION()
 
 template<BOOST_PP_ENUM_PARAMS(n, class CliType)>
-void bind_parameters(BOOST_PP_ENUM_BINARY_PARAMS(n, const CliType, &value)) {
+void bind_parameters(BOOST_PP_ENUM_BINARY_PARAMS(n, CliType, &value)) {
   // compilation error here probably indicates type mismatch between placeholders and parameters
-  bind_parameters_(fusion::vector<BOOST_PP_REPEAT(n, BOOST_RDB_ADD_REF, const CliType)>(BOOST_PP_ENUM_PARAMS(n, value)));
+  bind_parameters_(fusion::vector<BOOST_PP_REPEAT(n, BOOST_RDB_ADD_REF, CliType)>(BOOST_PP_ENUM_PARAMS(n, value)));
 }
