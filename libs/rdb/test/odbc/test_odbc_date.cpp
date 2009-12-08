08213452ca86f2eadebdd283688db4059dd8ba2b
jl <jl@yorel.be>
2009-12-08 18:17:00 +0100

    binding datetie to varchar works

--- libs/rdb/test/odbc/test_odbc_date.cpp
+++ libs/rdb/test/odbc/test_odbc_date.cpp
@@ -1,5 +1,4 @@
 #include "test_odbc.hpp"
-#include <boost/rdb/sql/dynamic.hpp>
 #include <boost/test/unit_test.hpp>
 
 using namespace std;
@@ -15,23 +14,44 @@ BOOST_RDB_BEGIN_TABLE(test1)
   BOOST_RDB_COLUMN(val, datetime)
 BOOST_RDB_END_TABLE(test1)
 
+struct fixture {
+
+  boost::rdb::odbc::database db;
+
+  fixture() : db("boost", "boost", "boost") {
+    test1 test;
+    try {
+      db.execute(create_table(test));
+    } catch (odbc_error) {
+      try {
+        db.execute(drop_table(test));
+      } catch (...) {
+      }
+      db.execute(create_table(test));
+    }
+  }
+
+  ~fixture() {
+    using namespace boost::rdb::sql::test::springfield;
+    db.execute(drop_table(test1::_));
+  }
+};
+
 }
 
-BOOST_AUTO_TEST_CASE(date_type) {
-  
-  using sql::select;
-  
-  database db("boost", "boost", "boost");
-  test1 test;
+BOOST_FIXTURE_TEST_CASE(date_type, fixture) {
 
-  try {
-    db.execute(create_table(test));
-  } catch (odbc_error) {
-    try {
-      db.execute(drop_table(test));
-    } catch (...) {
-    }
-    db.execute(create_table(test));
-  }
-  
+  using sql::select;
+
+  test1 t;
+
+  db.execute(insert_into(t)(t.id, t.val).values(1, "1963-08-13 03:11:17"));
+  BOOST_RDB_CHECK_SELECT_RESULTS(db.execute(select(t.id).from(t)), "((1))");
+
+  BOOST_AUTO(st, db.prepare(select(t.val).from(t)));
+  varchar<30> val;
+  st.bind_results(val);
+  st.execute().fetch();
+  BOOST_CHECK(!val.is_null());
+  BOOST_CHECK_EQUAL(val.value(), "1963-08-13 03:11:17");
 }
